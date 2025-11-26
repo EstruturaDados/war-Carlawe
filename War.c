58af695c765cf813329b3c570b6ce91cd77f638f
Carlawe <freitaswalle05@gmail.com>
2025-11-26 23:22:59 +0000

    ok

--- War.c
+++ War.c
@@ -26,84 +26,86 @@ struct Territorio meuterritorio5;
 printf("******CADASTRO DOS TERRITÓRIOS******\n");
 printf("\n");
 printf("******Território 1******\n");
-printf("Digite o nome do  território 1:");
+printf("Nome do  território: ");
 scanf("%s", meuterritorio1.nome);
-printf("Digite a cor do terrritorio 1:");
+printf("Cor do Exercito: ");
 scanf("%s", &meuterritorio1.cor);
-printf("Digite a quantidade de tropas do territorio 1:");
+printf("Número de tropas: ");
 scanf("%d", &meuterritorio1.tropas);
 printf("\n");
 
 printf("******Território 2******\n");
-printf("Digite o nome do  território 2:");
+printf("Nome do  território: ");
 scanf("%s", meuterritorio2.nome);
-printf("Digite a cor do terrritorio 2:");
+printf("Cor do exército: ");
 scanf("%s", &meuterritorio2.cor);
-printf("Digite a quantidade de tropas do territorio 2:");
+printf("Número de tropas: ");
 scanf("%d", &meuterritorio2.tropas);
 printf("\n");
 
 printf("******Território 3******\n");
-printf("Digite o nome do  território 3:");
+printf("Nome do  território: ");
 scanf("%s", meuterritorio3.nome);
-printf("Digite a cor do terrritorio 3:");
+printf("Cor do exército: ");
 scanf("%s", &meuterritorio3.cor);
-printf("Digite a quantidade de tropas do territorio 3:");
+printf("Número de tropas: ");
 scanf("%d", &meuterritorio3.tropas);
 printf("\n");
 
 printf("******Território 4******\n");
-printf("Digite o nome do  território 4:");
+printf("Nome do  território: ");
 scanf("%s", meuterritorio4.nome);
-printf("Digite a cor do terrritorio 4:");
+printf("Cor do exército: ");
 scanf("%s", &meuterritorio4.cor);
-printf("Digite a quantidade de tropas do territorio 4:");
+printf("Número de tropas: ");
 scanf("%d", &meuterritorio4.tropas);
 printf("\n");
 
 printf("******Território 5******\n");
-printf("Digite o nome do  território 5:");
+printf("Nome do  território: ");
 scanf("%s", meuterritorio5.nome);
-printf("Digite a cor do terrritorio 5:");
+printf("Cor do exército: ");
 scanf("%s", &meuterritorio5.cor);
-printf("Digite a quantidade de tropas do territorio 5:");
+printf("Número de tropas: ");
 scanf("%d", &meuterritorio5.tropas);
 printf("\n");
 printf("Cadastro concluido!!!");
 printf("\n");
 
 //Saída dos dados.
-
-printf("******Dados dos Territórios*******\n");
-
+printf("========================================\n");
+printf("     MAPA DO MUNDO - ESTADO ATUAL\n");
+printf("========================================\n");
+printf("\n");
 printf("Território 1:\n");
-printf("nome: %s\n", meuterritorio1.nome);
-printf("Dominado por: Exército %s\n", meuterritorio1.cor);
-printf("Quantidade de tropas: %d\n", meuterritorio1.tropas);
+printf("- Nome: %s\n", meuterritorio1.nome);
+printf("- Dominado por: Exército %s\n", meuterritorio1.cor);
+printf("- Quantidade de tropas: %d\n", meuterritorio1.tropas);
 printf("\n");
 
 printf("Território 2:\n");
-printf("nome: %s\n", meuterritorio2.nome);
-printf("Dominado por: Exército: %s\n", meuterritorio2.cor);
-printf("Quantidade de tropas: %d\n", meuterritorio2.tropas);
+printf("- Nome: %s\n", meuterritorio2.nome);
+printf("- Dominado por: Exército %s\n", meuterritorio2.cor);
+printf("- Quantidade de tropas: %d\n", meuterritorio2.tropas);
 printf("\n");
 
 printf("Território 3:\n");
-printf("nome: %s\n", meuterritorio3.nome);
-printf("Dominado por: Exército: %s\n", meuterritorio3.cor);
-printf("Quantidade de tropas: %d\n", meuterritorio3.tropas);
+printf("- Nome: %s\n", meuterritorio3.nome);
+printf("- Dominado por: Exército %s\n", meuterritorio3.cor);
+printf("- Quantidade de tropas: %d\n", meuterritorio3.tropas);
 printf("\n");
 
 printf("Território 4:\n");
-printf("nome: %s\n", meuterritorio4.nome);
-printf("Dominado por: Exército: %s\n", meuterritorio4.cor);
-printf("Quantidade de tropas: %d\n", meuterritorio4.tropas);
+printf("- Nome: %s\n", meuterritorio4.nome);
+printf("- Dominado por: Exército %s\n", meuterritorio4.cor);
+printf("- Quantidade de tropas: %d\n", meuterritorio4.tropas);
 printf("\n");
 
 printf("Território 5:\n");
-printf("nome: %s\n", meuterritorio5.nome);
-printf("Dominado por: Exército: %s\n", meuterritorio5.cor);
-printf("Quantidade de tropas: %d\n", meuterritorio5.tropas);
+printf("- Nome: %s\n", meuterritorio5.nome);
+printf("- Dominado por: Exército %s\n", meuterritorio5.cor);
+printf("- Quantidade de tropas: %d\n", meuterritorio5.tropas);
+printf("\n");
 
 
 return 0;
